713bcc39f08eb44c9d174a07913a0edd99b30de4
Guilherme David <137658009+Guilherme-David@users.noreply.github.com>
2024-04-16 10:44:21 -0300

    Update reais_em_dolares.py

--- reais_em_dolares.py
+++ reais_em_dolares.py
@@ -1,6 +1,9 @@
 import api_cotacoes # importa biblioteca
 
-carteira = float(input('Quanto tem na carteira? R$ '))
+try:
+  carteira = float(input('Quanto tem na carteira? R$ '))
 
-reais_em_dolares = api_cotacoes.converter_reais_em_dolares(carteira)
-print(f'A quantidade de R$ {carteira:,.2f}, em Dólares é: USD {reais_em_dolares:,.2f}')
+  reais_em_dolares = api_cotacoes.converter_reais_em_dolares(carteira)
+  print(f'A quantidade de R$ {carteira:,.2f}, em Dólares é: USD {reais_em_dolares:,.2f}')
+except ValueError:
+  print('Valor informado foi inválido. \nEsperava um valor em unidade monetária.
